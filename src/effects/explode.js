7cc68b3c4d340d4da11c629b649f8723e3f21c1c
Andrew Reida <andrew@synapsestudios.com>
2020-02-19 18:29:41 -0700

    Unnecessary parenthesis

--- src/effects/explode.js
+++ src/effects/explode.js
@@ -22,43 +22,43 @@ module.exports = async (nextFrame, blank, original, index, height, width) => {
       );
     case 5:
       explosionFrame = await getExplosionFrame(1, height, width);
-      cropHeight = (height) * .1;
-      cropWidth = (width) * .1;
+      cropHeight = height * .1;
+      cropWidth = width * .1;
       break;
     case 6:
       explosionFrame = await getExplosionFrame(2, height, width);
-      cropHeight = (height) * .1;
-      cropWidth = (width) * .1;
+      cropHeight = height * .1;
+      cropWidth = width * .1;
       break;
     case 7:
       explosionFrame = await getExplosionFrame(3, height, width);
-      cropHeight = (height) * .2;
-      cropWidth = (width) * .2;
+      cropHeight = height * .2;
+      cropWidth = width * .2;
       break;
     case 8:
       explosionFrame = await getExplosionFrame(4, height, width);
-      cropHeight = (height) * .2;
-      cropWidth = (width) * .2;
+      cropHeight = height * .2;
+      cropWidth = width * .2;
       break;
     case 9:
       explosionFrame = await getExplosionFrame(5, height, width);
-      cropHeight = (height) * .3;
-      cropWidth = (width) * .3;
+      cropHeight = height * .3;
+      cropWidth = width * .3;
       break;
     case 10:
       explosionFrame = await getExplosionFrame(6, height, width);
-      cropHeight = (height) * .3;
-      cropWidth = (width) * .3;
+      cropHeight = height * .3;
+      cropWidth = width * .3;
       break;
     case 11:
       explosionFrame = await getExplosionFrame(7, height, width);
-      cropHeight = (height) * .4;
-      cropWidth = (width) * .4;
+      cropHeight = height * .4;
+      cropWidth = width * .4;
       break;
     case 12:
       explosionFrame = await getExplosionFrame(8, height, width);
-      cropHeight = (height) * .4;
-      cropWidth = (width) * .4;
+      cropHeight = height * .4;
+      cropWidth = width * .4;
       break;
     case 13:
       explosionFrame = await getExplosionFrame(9, height, width);
